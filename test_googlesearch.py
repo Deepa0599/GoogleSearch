5a99002095e646634aeb53fb448a656f97581c28
Deepa0599 <gadhavedeepa07@gmail.com>
2022-03-30 15:30:52 +0530

    Completed

---
+++ test_googlesearch.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+print("test case Started")
+driver.maximize_window()
+driver.get("https://www.google.com/")
+time.sleep(1)
+driver.find_element_by_name("q").send_keys("Harman")
+
+
+
+time.sleep(5)
+driver.close()
+print("Test case has successfully completed")
